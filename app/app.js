10168bb16f0f6f1b126e8daf42f601937c6469c9
Blaise Roberts <blaiseroberts@Blaises-MacBook-Pro-2.local>
2017-02-21 10:50:48 -0600

    got everything working, didn't use angular-route or angularfire yet

--- app/app.js
+++ app/app.js
@@ -1,1 +1,28 @@
 "use strict";
+
+var app = angular.module("MushroomApp", []);
+
+app.controller("MushroomCtrl", function($scope, $http){
+    $http({
+	  method: 'GET',
+	  url: '../mushroom.json'
+	}).then(function successCallback(response) {
+	    let newArray = [];
+	    for (var i in response.data.mushrooms) {
+	    	let newObj = {};
+	    	newObj.name = Object.keys(response.data.mushrooms[i]);
+	    	newObj.description = response.data.mushrooms[i][newObj.name].description;
+	    	newObj.edible = response.data.mushrooms[i][newObj.name].edible;
+	    	newObj.color = newObj.edible ? "success" : "danger";
+	    	newArray.push(newObj);
+
+
+	    }
+	  	$scope.mushrooms = newArray;
+	  	console.log(newArray);
+
+	  }, function errorCallback(response) {
+	    // called asynchronously if an error occurs
+	    // or server returns response with an error status.
+	  });
+	});
